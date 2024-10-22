130c6ade904781a3509b13876088075e89307d78
oladotun <oladotunadekomaya@gmail.com>
2024-10-22 03:34:04 +0100

    commit

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from dotenv import load_dotenv 
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-import google.generativeai as genai
+# import google.generativeai as genai
 
 load_dotenv()
 
-# geminiapi_key = os.getenv('geminiapi_key')
+geminiapi_key = os.getenv('geminiapi_key')
 
-
-
-# llm = ChatGoogleGenerativeAI(model="gemini-pro")
-# llm.invoke("Sing a ballad of LangChain.")
 
 # genai.configure(api_key=geminiapi_key)
 
-print(os.environ)
+# model = genai.GenerativeModel("gemini-1.5-flash")
 
-# get a list of all models in gemini
+os.environ['GOOGLE_API_KEY']=geminiapi_key
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-002",temperature=0.9)
+response = llm.invoke("Sing a ballad of LangChain.")
+print(response.content)
+
+
+
+
+
+# Check the list of models
+
 # for model in genai.list_models():
 #     print(model.name)
-
-# model = genai.GenerativeModel("gemini-1.5-pro-002")
-# response = model.generate_content("Write a story about a magic backpack.")
-# print(response.text)
-
-# llm = ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.9)
-
-# # Send a creative prompt to the LLM
-# response = llm.invoke('Write a paragraph about life on Mars in year 2100.')
-# print(response.content)
